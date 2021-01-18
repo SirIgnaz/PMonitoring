1804cf3e6284c889b9fd586c25c1d4280d530554
SirIgnaz <15999046+SirIgnaz@users.noreply.github.com>
2021-01-18 21:21:30 +0100

    Update Connection-Sensor.ps1

--- Sensors/Connection-Sensor.ps1
+++ Sensors/Connection-Sensor.ps1
@@ -1,8 +1,21 @@
-$Destination = Read-Host -Prompt "Input Destination"
-Write-Host -Object "Resolve Destination" -ForegroundColor Green
-$ResolveDestination = Test-Connection $Destination -ResolveDestination | Select-Object -First 1
-Write-Host -Object $ResolveDestination.Address
-  
-Write-Host -Object "Check MTU Size" -ForegroundColor Green
-$MTUSize = Test-Connection $Destination -MTUSize
-Write-Host -Object $MTUSize.MtuSize
+#Requires -RunAsAdministrator
+#Requires -Version 7
+
+$Target = Read-Host "Please input Name or IP Address"
+
+if ((Test-NetConnection $Target ).PingSucceeded){
+
+$MTUSize = Test-Connection -TargetName $Target -MtuSize
+$DNSName = Resolve-DnsName -Name $Target
+$DiagnosticRouting = Test-NetConnection -ComputerName $Target -DiagnoseRouting -InformationLevel Detailed
+
+Write-Host "MTU Size" -ForegroundColor Green
+Write-Host $MTUSize.MtuSize
+Write-Host "DNS" -ForegroundColor Green
+Write-Host $DNSName.Name
+Write-Host "Outgoing Interface" -ForegroundColor Green
+Write-Host $DiagnosticRouting.OutgoingInterfaceAlias
+}
+else{
+    Write-Error "Target not reachable"
+}
